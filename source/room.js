54b692c14b73230364ba6b39e30f80cf9f5bdb20
Christopher Hall <Biomech.ArkahnX@gmail.com>
2014-08-18 12:05:52 -0700

    JS13KGames Day Six
    Worked on room generation algorithm. Connects decently to rooms above,
    but doesnt account for rooms below.

--- source/room.js
+++ source/room.js
@@ -30,10 +30,10 @@ var room6 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0
 var room7 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0, 1, 1, 1, 0, 1, 1, 0, 0, 0, 0, 1, 1, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1];
 var room8 = [1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
 var room9 = [1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1];
-var room10 = [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0];
-var room11 = [0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1];
-var room12 = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
-var room13 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 1, 1, 1, 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+var room10 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 2, 2, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+var room11 = [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1, 0, 0, 0, 0, 0, 0, 2, 2, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1];
+var room12 = [1, 1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1];
+var room13 = [1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 1, 1, 1];
 
 function Room(flipX, flipY, rotate, RoomType, doors, paths, array) {
 	return {
@@ -92,9 +92,7 @@ function BigRoom(width, height, flipX, flipY, rotate, RoomType, doors, paths, ro
 				var rotation = null;
 				if (!rooms[roomId]) {
 					rotation = random(0, 3);
-					// room = cloneRoom(roomList[9]);
 					room = cloneRoom(roomList[array[arrayIndex]]);
-					// console.log(roomId, array[arrayIndex], room.map)
 					if (room.options & allowRotate) {
 						rooms[roomId] = rotate(room, roomSize, roomSize, rotation);
 					} else {
@@ -103,20 +101,7 @@ function BigRoom(width, height, flipX, flipY, rotate, RoomType, doors, paths, ro
 				}
 				room = rooms[roomId];
 			}
-			console.log(coordinate(x, y, topSize * roomSize), coordinate(x % roomSize, y % roomSize, roomSize))
 			map[coordinate(x, y, topSize * roomSize)] = room.map[coordinate(x % roomSize, y % roomSize, roomSize)];
-
-			// 	if (x % roomSize === 0) {
-			// 		currentX = currentX + 1;
-			// 	}
-			// 	if (currentX > width - 1) {
-			// 		currentX = 0;
-			// 		currentY++;
-			// 	}
-			// 	var roomCoordinate = coordinate(currentX, currentY, topSize);
-			// 	// console.log(x, y, currentX, currentY, roomCoordinate, array[roomCoordinate], width, height)
-			// 	}
-			// }
 		}
 	}
 	currentMapTiles = topSize * roomSize;
@@ -136,75 +121,226 @@ function BigRoom(width, height, flipX, flipY, rotate, RoomType, doors, paths, ro
 	};
 }
 
-bigRoomList.push(BigRoom(2, 2, 0, 0, 0, 0, 0, 1 | 2 | 4 | 8, function(array, roomsX, roomsY, arraySize) {
-	var startX = random(0, roomsX - 1);
-	var startY = random(0, roomsY - 1);
-	var startDirection = random(0, 3);
-	var currentX = startX;
-	var currentY = startY;
-	if (startDirection === 0) { // door is in the left wall
-		array[coordinate(startX, startY, arraySize)] = 10;
+function setRoom(startX, startY, currentX, currentY, arraySize, array, validRooms, roomSelection, roomsX, roomsY) {
+	roomSelection.length = 0;
+	for (var e = 0; e < roomList.length; e++) {
+		validRooms[e] = e;
 	}
-	if (startDirection === 1) { // door is in the top wall
-		array[coordinate(startX, startY, arraySize)] = 13;
-
+	var aboveRoom = array[coordinate(currentX, currentY - 1, arraySize)];
+	var leftRoom = array[coordinate(currentX - 1, currentY, arraySize)];
+	var rightRoom = array[coordinate(currentX + 1, currentY, arraySize)];
+	var belowRoom = array[coordinate(currentX, currentY + 1, arraySize)];
+	if (currentY - 1 < 0) {
+		aboveRoom = -1;
 	}
-	if (startDirection === 2) { // door is in the right wall
-		array[coordinate(startX, startY, arraySize)] = 11;
+	if (currentX - 1 < 0) {
+		leftRoom = -1;
 	}
-	if (startDirection === 3) { // door is in the bottom wall
-		array[coordinate(startX, startY, arraySize)] = 12;
+	if (currentY + 1 > roomsY - 1) {
+		belowRoom = -1;
 	}
-	var path = true;
-	var visited = [startX + "-" + startY];
-	var previousX = startX;
-	var previousY = startY;
-	var previousRoom = roomList[array[coordinate(startX, startY, arraySize)]];
-	var attempts = 0;
-	while (path && attempts < 4) {
-		var direction = random(1, 5);
-		if (direction === 2 || direction === 1) {
-			currentX++;
-		}
-		if (direction === 4 || direction === 3) {
-			currentX--;
-		}
-		if (direction === 5) {
-			currentY++;
-		}
-		if (currentX > roomsX - 1) {
-			currentY++;
-			currentX = roomsX - 1;
-		}
-		if (currentX < 0) {
-			currentY++;
-			currentX = 0;
-		}
-		if (currentY > roomsY - 1) {
-			currentY = roomsY - 1;
-		}
-		if (currentY < 0) {
-			currentY = 0;
-		}
-		if (visited.indexOf(currentX + "-" + currentY) === -1) {
-			console.log()
-			array[coordinate(currentX, currentY, arraySize)] = 9;
-			previousRoom = roomList[array[coordinate(currentX, currentY, arraySize)]];
-			previousX = currentX;
-			previousY = currentY;
-			visited.push(currentX + "-" + currentY);
-		} else {
-			attempts++;
-		}
-
+	if (currentX + 1 > roomsX - 1) {
+		rightRoom = -1;
 	}
-
-	for (var x = 0; x < roomsX; x++) {
-		for (var y = 0; y < roomsY; y++) {
-			if (x === 0 && y === 0) {
-
+	if (aboveRoom === 4 || aboveRoom === 2 || aboveRoom === 6 || aboveRoom === 7 || aboveRoom === 9 || aboveRoom === 13) {
+		console.warn("Room for Above")
+		for (var i = 0; i < validRooms.length; i++) {
+			validRooms[i] = -1;
+		}
+		validRooms[2] = 2;
+		validRooms[3] = 3;
+		validRooms[5] = 5;
+		validRooms[8] = 8;
+		validRooms[9] = 9;
+		if (leftRoom !== -1) {
+			if (leftRoom === 1 || leftRoom === 3 || leftRoom === 4 || leftRoom === 5 || leftRoom === 6 || leftRoom === 9 || leftRoom === 10) {
+				console.warn("Room for Above and Left")
+				validRooms[2] = -1;
+				validRooms[5] = -1;
+				validRooms[3] = 3;
+				validRooms[8] = 8;
+				validRooms[9] = 9;
+				if (rightRoom !== -1) {
+					if (rightRoom === 1 || rightRoom === 3 || rightRoom === 4 || rightRoom === 7 || rightRoom === 8 || rightRoom === 9 || rightRoom === 11) {
+						validRooms[8] = -1;
+					}
+				}
+			}
+		}
+		if (rightRoom !== -1) {
+			if (rightRoom === 1 || rightRoom === 3 || rightRoom === 4 || rightRoom === 7 || rightRoom === 8 || rightRoom === 9 || rightRoom === 11) {
+				console.warn("Room for Above and Left")
+				validRooms[2] = -1;
+				validRooms[8] = -1;
+				validRooms[3] = 3;
+				validRooms[5] = 5;
+				validRooms[9] = 9;
+				if (leftRoom !== -1) {
+					if (leftRoom === 1 || leftRoom === 3 || leftRoom === 4 || leftRoom === 5 || leftRoom === 6 || leftRoom === 9 || leftRoom === 10) {
+						validRooms[5] = -1;
+					}
+				}
 			}
 		}
 	}
+	if (currentX === 0) {
+		console.warn("Left of Map")
+		validRooms[7] = -1;
+		validRooms[8] = -1;
+		validRooms[1] = -1;
+	}
+	if (currentX === roomsX - 1) {
+		console.warn("Right of Map")
+		validRooms[5] = -1;
+		validRooms[6] = -1;
+		validRooms[1] = -1;
+	}
+	if (currentY === 0) {
+		console.warn("Top of Map")
+		validRooms[3] = -1;
+		validRooms[5] = -1;
+		validRooms[8] = -1;
+		validRooms[2] = -1;
+	}
+	if (currentY === roomsY - 1) {
+		console.warn("Bottom of Map")
+		validRooms[4] = -1;
+		validRooms[6] = -1;
+		validRooms[7] = -1;
+		validRooms[2] = -1;
+	}
+	if (currentX === startX && currentY === startY) {
+		console.warn("Room for Start")
+		for (var i = 0; i < validRooms.length; i++) {
+			validRooms[i] = -1;
+		}
+		validRooms[10] = 10;
+		validRooms[11] = 11;
+		validRooms[12] = 12;
+		validRooms[13] = 13;
+		if (leftRoom === -1) {
+			validRooms[11] = -1;
+		}
+		if (aboveRoom === -1) {
+			validRooms[12] = -1;
+		}
+		if (rightRoom === -1) {
+			validRooms[10] = -1;
+		}
+		if (belowRoom === -1) {
+			validRooms[13] = -1;
+		}
+	} else {
+		validRooms[10] = -1;
+		validRooms[11] = -1;
+		validRooms[12] = -1;
+		validRooms[13] = -1;
+	}
+	validRooms[0] = 0;
+	for (var i = 0; i < validRooms.length; i++) {
+		if (validRooms[i] !== -1) {
+			roomSelection.push(validRooms[i]);
+		}
+	}
+	var selectedRoom = roomSelection[random(0, roomSelection.length - 1)];
+	if (selectedRoom === 11) {
+		if ([1, 3, 4, 5, 6, 9].indexOf(leftRoom) === -1) {
+			setRoom(startX, startY, currentX - 1, currentY, arraySize, array, validRooms, roomSelection, roomsX, roomsY);
+		}
+	}
+	if (selectedRoom === 12) {
+		if ([2, 4, 6, 7, 9].indexOf(aboveRoom) === -1) {
+			setRoom(startX, startY, currentX, currentY - 1, arraySize, array, validRooms, roomSelection, roomsX, roomsY);
+		}
+	}
+	console.log(selectedRoom, roomSelection, array, startX, startY)
+	array[coordinate(currentX, currentY, arraySize)] = selectedRoom;
+}
+
+bigRoomList.push(BigRoom(4, 4, 0, 0, 0, 0, 0, 1 | 2 | 4 | 8, function(array, roomsX, roomsY, arraySize) {
+	var startX = random(0, roomsX - 1);
+	var startY = random(0, roomsY - 1);
+	var startDirection = random(0, 3);
+	var currentX = 0;
+	var currentY = 0;
+	var validRooms = [];
+	var roomSelection = [];
+	for (var i = 0; i < arraySize * arraySize; i++) {
+		setRoom(startX, startY, currentX, currentY, arraySize, array, validRooms, roomSelection, roomsX, roomsY);
+		currentX++;
+		if (currentX > roomsX - 1) {
+			currentX = 0;
+			currentY++;
+		}
+		if (currentY > roomsY - 1) {
+			i = arraySize * arraySize;
+		}
+	}
+	// var currentX = startX;
+	// var currentY = startY;
+	// if (startDirection === 0) { // door is in the left wall
+	// 	array[coordinate(startX, startY, arraySize)] = 10;
+	// }
+	// if (startDirection === 1) { // door is in the top wall
+	// 	array[coordinate(startX, startY, arraySize)] = 13;
+
+	// }
+	// if (startDirection === 2) { // door is in the right wall
+	// 	array[coordinate(startX, startY, arraySize)] = 11;
+	// }
+	// if (startDirection === 3) { // door is in the bottom wall
+	// 	array[coordinate(startX, startY, arraySize)] = 12;
+	// }
+	// var path = true;
+	// var visited = [startX + "-" + startY];
+	// var previousX = startX;
+	// var previousY = startY;
+	// var previousRoom = roomList[array[coordinate(startX, startY, arraySize)]];
+	// var attempts = 0;
+	// while (path && attempts < 4) {
+	// 	var direction = random(1, 5);
+	// 	if (direction === 2 || direction === 1) {
+	// 		currentX++;
+	// 	}
+	// 	if (direction === 4 || direction === 3) {
+	// 		currentX--;
+	// 	}
+	// 	if (direction === 5) {
+	// 		currentY++;
+	// 	}
+	// 	if (currentX > roomsX - 1) {
+	// 		currentY++;
+	// 		currentX = roomsX - 1;
+	// 	}
+	// 	if (currentX < 0) {
+	// 		currentY++;
+	// 		currentX = 0;
+	// 	}
+	// 	if (currentY > roomsY - 1) {
+	// 		currentY = roomsY - 1;
+	// 	}
+	// 	if (currentY < 0) {
+	// 		currentY = 0;
+	// 	}
+	// 	if (visited.indexOf(currentX + "-" + currentY) === -1) {
+	// 		console.log()
+	// 		array[coordinate(currentX, currentY, arraySize)] = 9;
+	// 		previousRoom = roomList[array[coordinate(currentX, currentY, arraySize)]];
+	// 		previousX = currentX;
+	// 		previousY = currentY;
+	// 		visited.push(currentX + "-" + currentY);
+	// 	} else {
+	// 		attempts++;
+	// 	}
+
+	// }
+
+	// for (var x = 0; x < roomsX; x++) {
+	// 	for (var y = 0; y < roomsY; y++) {
+	// 		if (x === 0 && y === 0) {
+
+	// 		}
+	// 	}
+	// }
 	return array;
 }));
